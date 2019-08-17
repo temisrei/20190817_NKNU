33e91c2138759120e2f67acd35e3879d7027f4f3
Rei Lin <temisrei@gmail.com>
2019-08-17 15:53:31 +0800

    簡單的預測，輸入變項為後端測資

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+
+# 新增載入模組
+from sklearn.externals import joblib
+import numpy as np
 
 app = Flask(__name__)
+
+# 測試資料
+# iris = [5.1, 3.5, 1.4, 0.2] # Iris Setosa
+# iris = [7.0, 3.2, 4.7, 1.4] # Iris Versicolour
+iris = [6.4, 2.8, 5.6, 2.1] # Iris Virginica
+
+# 把模型讀進來
+classifier = joblib.load('classifier.joblib')
 
 @app.route('/', methods=['GET'])
 def main():
   return "<h1>Hello Flask!</h1>"
+
+@app.route('/api', methods=['GET'])
+def api():
+  # formData = request.json # 接資料
+  formData = jsonify(iris).json
+  # data = [val for val in formData.values()]
+  data = formData
+
+  # 預測資料
+  prediction = classifier.predict(np.array(data).reshape(1, -1))
+
+  # 輸出類別
+  types = { 0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
+
+  response = jsonify({
+    "statusCode": 200,
+    "status": "Prediction made",
+    "result": "The type of iris plant is: " + types[prediction[0]]
+  })
+  return response
 
 if __name__ == '__main__':
   app.run()
